73c97a51b5f0b8c1978c9493d9aeffc48fae8134
David <davidacumora@gmail.com>
2023-10-30 08:15:32 -0600

    AuthProvider, Driver log general changes

---
+++ src/pages/workingTimeControl/WorkingTimeDetails.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function WorkingTimeDetails() {
+  return (
+    <div>WorkingTimeDetails</div>
+  )
+}
+
+export default WorkingTimeDetails
--- src/services/DriverLogService.js
+++ src/services/DriverLogService.js
@@ -1,13 +1,8 @@
 import api from "../api/config";
 
 export const create = async (driverLog) => { 
-    if (driverLog.id) {
-        let data = await api.put(`driverLogs/${driverLog.id}`, driverLog).then(result => result.data);
-        return data;
-    } else {
         let data = await api.post('driverLogs', driverLog).then(result => result.data);
         return data;
-    }
 };
 
 export const getDriverLog = async () => { 
