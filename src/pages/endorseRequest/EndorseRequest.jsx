f3cf5c985dbfd46d0687bb7118fa636fea3db264
David <davidacumora@gmail.com>
2024-04-25 22:07:10 -0600

    isCancelled deleted

--- src/pages/endorseRequest/EndorseRequest.jsx
+++ src/pages/endorseRequest/EndorseRequest.jsx
@@ -524,7 +524,7 @@ function EndorseRequest() {
                     //  const isCanceled = data.filter(item => item.ItsCanceled == true);
 
                     // Si el vehículo está disponible, mostrarlo en la lista
-                    if (isAvailable || isCanceled) {
+                    if (isAvailable) {
                       return (
                         <option key={vehicle.id} value={vehicle.id}>
                           {vehicle.plate_Number} - {vehicle.category}
